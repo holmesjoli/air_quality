c5061182afe6b8af1db5b1858653d2339adc70a8
Joli Holmes <holmesjoli@gmail.com>
2021-10-01 15:09:19 -0400

    reorganized file

--- R/helper_functions.R
+++
@@ -1,9 +0,0 @@
-#' @title Get AQS data
-#' @description AQS: air quality system
-get_aqs_df <- function(url) {
-
-  httr::GET(url) %>% 
-    httr::content() %>% 
-    .[[2]] %>% 
-    dplyr::bind_rows()
-}
--- main.R
+++
@@ -1,5 +0,0 @@
-library(magrittr)
-
-config <- yaml::read_yaml("config.yaml")
-
-assertthat::assert_that(httr::status_code(httr::GET(config$api_up)) == 200)
